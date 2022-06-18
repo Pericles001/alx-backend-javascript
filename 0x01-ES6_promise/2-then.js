02defff32767f2107e5d00a67f75bffaeee59287
pericles001 <periclesadjovi@outlook.fr>
2022-06-18 11:25:10 +0100

    Fix braces

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,3 +1,3 @@
 export default function handleResponseFromAPI(promise) {
-  return promise.then(() => ({ status: 200, body: 'success' })).catch(() => { Error(); }).finally(() => { console.log('Got a response from the API'); });
+  return promise.then(() => ({ status: 200, body: 'success' })).catch(() => Error()).finally(() => { console.log('Got a response from the API'); });
 }
