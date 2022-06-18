b8330b8bb15124ea3f1b8fc02cb71e2dda683f88
pericles001 <periclesadjovi@outlook.fr>
2022-06-18 12:08:15 +0100

    Edit file

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   const upload = uploadPhoto();
   const create = createUser();
-  Promise.all([upload, create]).then((result) => {
+  return Promise.all([upload, create]).then((result) => {
     console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
   }).catch(() => {
     console.log('Signup system offline');
