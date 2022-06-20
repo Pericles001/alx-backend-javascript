709478f9f57d25a1ffb3640c2bb1c42fdea3d2cd
pericles001 <periclesadjovi@outlook.fr>
2022-06-20 01:44:53 +0100

    Edit file with currency

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,28 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof code !== 'string') throw new Error();
+    if (typeof name !== 'string') throw new Error();
+    this.code = code;
+    this.name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(val) {
+    this._code = val;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(val) {
+    this._name = val;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
