16628829d18b4c8b2c68e6fdc85ce0e6ca2e8da2
pericles001 <periclesadjovi@outlook.fr>
2022-06-21 01:55:23 +0100

    Fix return statement

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -3,5 +3,5 @@ export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const arr = new Int8Array(buffer);
   arr[position] = value;
-  return arr;
+  return buffer;
 }
