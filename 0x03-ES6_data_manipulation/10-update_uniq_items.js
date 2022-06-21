1a2a6d7dbba149343ae45cc7b919cd4feadf89b4
pericles001 <periclesadjovi@outlook.fr>
2022-06-21 03:44:58 +0100

    Update file

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,8 +1,6 @@
 export default function updateUniqueItems(map) {
   if ((!map) instanceof Map) throw Error('Cannot process');
-  Array.from(map.keys()).forEach((key) => {
-    if (map.get(key) === 1) {
-      map.set(key, 100);
-    }
+  map.forEach((value, key) => {
+    if (value === 1) map.set(key, 100);
   });
 }
