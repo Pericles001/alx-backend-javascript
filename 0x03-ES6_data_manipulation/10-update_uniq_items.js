573eba4ef9fcfc16664923d6f13082d18b54b8b3
pericles001 <periclesadjovi@outlook.fr>
2022-06-21 03:48:57 +0100

    Fix error thrower

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,6 +1,9 @@
-export default function updateUniqueItems(map) {
-  if ((!map) instanceof Map) throw new Error('Cannot process');
+const updateUniqueItems = (map) => {
+  if (!(map instanceof Map)) throw new Error('Cannot process');
+
   map.forEach((value, key) => {
     if (value === 1) map.set(key, 100);
   });
-}
+};
+
+export default updateUniqueItems;
