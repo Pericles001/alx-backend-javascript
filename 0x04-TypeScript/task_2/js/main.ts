ef004f3cec14b669fcf571d6cf8627c3b0b15b40
pericles001 <periclesadjovi@outlook.fr>
2022-06-23 00:59:09 +0100

    Update file -Create teachClass function

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -33,5 +33,23 @@ export class Teacher implements TeacherInterface {
 
 }
 
-const createEmployee = (salary: number | string) : Teacher | Director => Number(salary) < 500 ? new Teacher() : new Director();
+export const createEmployee = (salary: number | string): Teacher | Director => Number(salary) < 500 ? new Teacher() : new Director();
+
+export function isDirector(employee: TeacherInterface | DirectorInterface): employee is Director {
+    return (employee as Director).workDirectorTasks() !== undefined;
+}
+
+export function executeWork(employee: DirectorInterface | TeacherInterface): string {
+    let res = undefined;
+    (isDirector(employee)) ? res = employee.workDirectorTasks() : res = employee.workTeacherTasks();
+    return res;
+}
+
+type Subjects = "Math" | "History";
+
+// @ts-ignore
+// @ts-ignore
+export function teachClass(todayClass: Subjects): string | boolean {
+    return (todayClass === "Math") ? "Teaching Math" : (todayClass === "History") ? "Teaching History" : `${undefined}`
+}
 
