e5963b717714be27acdbb40a3d3645ee173e3bed
Pericles001 <periclesadjovi@gmail.com>
2022-08-10 02:13:17 +0100

    Edit file with solution

--- 0x05-Node_JS_basic/3-main_1.js
+++ 0x05-Node_JS_basic/3-main_1.js
@@ -1,9 +1,10 @@
 const countStudents = require('./3-read_file_async');
 
-countStudents("nope.csv")
+countStudents("database.csv")
     .then(() => {
         console.log("Done!");
     })
         .catch((error) => {
         console.log(error);
     });
+console.log("After!");
